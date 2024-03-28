b1d6274f3e7f3fbd9ed6f1587d5958550fbb7a85
uxeeCoder <umar.afar@aol.com>
2024-03-27 22:36:41 -0400

    File created to define object asking users for their favorite programming language

---
+++ questions/favoriteLanguage.js
@@ -0,0 +1,11 @@
+"use strict"   ;
+
+
+const favLanguage = {
+    type: "list",
+    name: "languages",
+    message: "What is your favorite programming language?",
+    choices: ["Python", "JavaScript", "Java", "Ruby", "C++", "Other"],
+
+};
+export default favLanguage;
