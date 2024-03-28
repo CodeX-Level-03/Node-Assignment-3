d0059d2642b9c539ba9018f6bf6951d316fabfc7
uxeeCoder <umar.afar@aol.com>
2024-03-27 22:37:02 -0400

    File created to define object asking users for their favorite Editing Tool

---
+++ questions/favoriteTool.js
@@ -0,0 +1,8 @@
+"use strict";
+const favTool = {
+    "type": "input",
+    "name": "preferredTool",
+    "message": "Which editor do you prefer as a development tool?",
+
+};
+export default favTool;
