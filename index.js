0ee4abeb35fe80f21c485b0eb18c64505a09e1ac
uxeeCoder <umar.afar@aol.com>
2024-03-27 22:29:06 -0400

    Main survey appkication with inquirer package and question modules

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict"    ;
+import inquirer from "inquirer";
+
+import favLanguage from "./questions/favoriteLanguage.js";
+import favTool from "./questions/favoriteTool.js";
+
+inquirer.prompt([favLanguage, favTool]).then((answers) => { 
+    console.log(`Your favorite programming language is ${answers.languages}, and you prefer ${answers.preferredTool} as a development tool. Your input is highly appreciated.`);
+});
